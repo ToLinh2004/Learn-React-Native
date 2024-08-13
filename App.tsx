afbf335f2899ea64b230809e8077a4cbb79e1f5c
Linh <linh.nguyen@sapotacorp.vn>
2024-08-14 06:29:49 +0700

    use arr, interactive with scrollview

--- App.tsx
+++ App.tsx
@@ -1,43 +1,35 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState<string>("");
-  const [age, setAge]=useState<number>();
-  const [test, setTest] = useState({
-    name: "Linh",
-    age: 25,
-  });
-  const [count, setCount] = useState(0);
+ const [students, setStudents] = useState([
+   { id: 1, name: "Bao1", age: 19 },
+   { id: 2, name: "Bao2", age: 19 },
+   { id: 3, name: "Bao3", age: 19 },
+   { id: 4, name: "Bao4", age: 19 },
+   { id: 5, name: "Ba05", age: 19 },
+   { id: 6, name: "Bao6", age: 19 },
+   { id: 7, name: "Bao7", age: 10 },
+   { id: 8, name: "Bao8", age: 20 },
+   { id: 9, name: "Bao9", age: 20 },
+   { id: 10, name: "Bao10", age: 20 },
+   { id: 11, name: "Bao11", age: 20 },
+   { id: 12, name: "Bao12", age: 20 },
+   { id: 13, name: "Bao13", age: 20 },
+ ]);
+ 
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Name: {name}</Text>
-        <TextInput
-          style={styles.styleInput}
-          multiline // automatic line break like textarea
-          onChangeText={(value) => setName(value)}
-        />
-      </View>
-      <View>
-        <Text>Age: {age}</Text>
-        <TextInput
-          style={styles.styleInput}
-          
-          onChangeText={(value) => setAge(+value)}
-          keyboardType="numeric"
-          maxLength={2}
-        />
-      </View>
-
-      <Text>Hello word</Text>
-      <Text>
-        {JSON.stringify(test)} {test.name}
-      </Text>
-      <Text>count = {count}</Text>
-      <View>
-        <Button title="Increase" onPress={() => setCount(count + 1)} />
-      </View>
+      <Text style={{fontSize:60}}>Hello word</Text>
+      <ScrollView >
+        {students.map(student =>{
+          return (
+            <View key={student.id} style={styles.listStudent}>
+              <Text>{student.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -46,13 +38,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop:50,
+    paddingHorizontal:20,
+
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  styleInput: {
-    borderWidth: 1,
-    color: "black",
-    width: 200,
-    padding: 12,
-  },
+  listStudent:{
+    padding:15,
+    backgroundColor:'purple',
+    marginBottom:30,
+  }
+ 
 });
